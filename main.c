35543a368734a580599a8b516186ecefdb05e1df
Mstislav2016 <mym@ranger.ru>
2026-01-17 20:41:45 +0300

    Update main.c
    English verion (im russian)

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <wchar.h>
 
-// Идентификаторы кнопок
+// Button IDs
 #define ID_LISTBOX   101
 #define ID_BTN_PLAY  102
 #define ID_BTN_STOP  103
@@ -18,7 +18,7 @@
 HWND hListBox;
 wchar_t folderPath[MAX_PATH];
 
-// Функция воспроизведения
+// Playback function
 void PlayMusic(const wchar_t* filename) {
     wchar_t command[2048];
     wchar_t fullPath[MAX_PATH];
@@ -27,8 +27,8 @@ void PlayMusic(const wchar_t* filename) {
     mciSendStringW(L"close mp3", NULL, 0, NULL);
     swprintf(fullPath, MAX_PATH, L"%ls\\%ls", folderPath, filename);
     
-    // Превращаем путь в короткий (DOS-формат), чтобы избежать проблем с пробелами и кириллицей в MCI
-    if (GetShortPathNameW(fullPath, shortPath, MAX_PATH) == 0) {
+    // We turn the path into a short (DOS format) to avoid problems with spaces and Cyrillic characters in MCI
+if (GetShortPathNameW(fullPath, shortPath, MAX_PATH) == 0) {
         swprintf(command, 2048, L"open \"%ls\" type mpegvideo alias mp3", fullPath);
     } else {
         swprintf(command, 2048, L"open %ls type mpegvideo alias mp3", shortPath);
@@ -40,7 +40,7 @@ void PlayMusic(const wchar_t* filename) {
     mciSendStringW(L"play mp3", NULL, 0, NULL);
 }
 
-// Загрузка песен из папки Super_music
+// Downloading songs from the Super_music folder
 void LoadPlaylist(HWND hList) {
     WIN32_FIND_DATAW findData;
     wchar_t searchPath[MAX_PATH];
@@ -52,26 +52,26 @@ void LoadPlaylist(HWND hList) {
             SendMessageW(hList, LB_ADDSTRING, 0, (LPARAM)findData.cFileName);
         } while (FindNextFileW(hFind, &findData));
         FindClose(hFind);
-        SendMessageW(hList, LB_SETCURSEL, 0, 0); // Выбираем первую песню по умолчанию
+        SendMessageW(hList, LB_SETCURSEL, 0, 0); // Selecting the first default song
     } else {
         CreateDirectoryW(folderPath, NULL);
     }
 }
 
-// Обработка логики окна
+// Window logic processing
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
-        case WM_CREATE: {
-            // Создаем кнопки
-            CreateWindowW(L"BUTTON", L"⭠ Назад", WS_VISIBLE | WS_CHILD, 10, 310, 70, 30, hwnd, (HMENU)ID_BTN_PREV, NULL, NULL);
-            CreateWindowW(L"BUTTON", L"▶ Играть", WS_VISIBLE | WS_CHILD, 85, 310, 70, 30, hwnd, (HMENU)ID_BTN_PLAY, NULL, NULL);
-            CreateWindowW(L"BUTTON", L"⏸ Стоп", WS_VISIBLE | WS_CHILD, 160, 310, 70, 30, hwnd, (HMENU)ID_BTN_STOP, NULL, NULL);
-            CreateWindowW(L"BUTTON", L"⭢ След.", WS_VISIBLE | WS_CHILD, 235, 310, 70, 30, hwnd, (HMENU)ID_BTN_NEXT, NULL, NULL);
+case WM_CREATE: {
+// Creating buttons
+            CreateWindowW(L"BUTTON", L"⭠ Back", WS_VISIBLE | WS_CHILD, 10, 310, 70, 30, hwnd, (HMENU)ID_BTN_PREV, NULL, NULL);
+            CreateWindowW(L"BUTTON", L"▶ Play", WS_VISIBLE | WS_CHILD, 85, 310, 70, 30, hwnd, (HMENU)ID_BTN_PLAY, NULL, NULL);
+            CreateWindowW(L"BUTTON", L"⏸ Stop", WS_VISIBLE | WS_CHILD, 160, 310, 70, 30, hwnd, (HMENU)ID_BTN_STOP, NULL, NULL);
+            CreateWindowW(L"BUTTON", L"⭢ Forward.", WS_VISIBLE | WS_CHILD, 235, 310, 70, 30, hwnd, (HMENU)ID_BTN_NEXT, NULL, NULL);
             
-            // Список файлов
+            // List of files
             hListBox = CreateWindowW(L"LISTBOX", NULL, WS_VISIBLE | WS_CHILD | LBS_NOTIFY | WS_VSCROLL | WS_BORDER, 10, 10, 295, 280, hwnd, (HMENU)ID_LISTBOX, NULL, NULL);
             
-            // Определяем путь к папке Super_music рядом с EXE
+            // Defining the path to the Super_music folder next to the EXE
             GetModuleFileNameW(NULL, folderPath, MAX_PATH);
             wchar_t* lastSlash = wcsrchr(folderPath, L'\\');
             if (lastSlash) *(lastSlash + 1) = L'\0';
@@ -96,7 +96,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             }
             if (LOWORD(wParam) == ID_BTN_NEXT) {
                 if (count > 0) {
-                    // Если это последняя песня (count-1), переходим на 0, иначе на sel+1
+                    // If this is the last song (count-1), switch to 0, otherwise to sel+1
                     int nextSel = (sel >= count - 1) ? 0 : sel + 1;
                     SendMessageW(hListBox, LB_SETCURSEL, nextSel, 0);
                     SendMessageW(hwnd, WM_COMMAND, ID_BTN_PLAY, 0);
@@ -104,7 +104,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             }
             if (LOWORD(wParam) == ID_BTN_PREV) {
                 if (count > 0) {
-                    // Если это первая песня (0), переходим на последнюю (count-1), иначе на sel-1
+                    // If this is the first song (0), switch to the last one (count-1), otherwise to sel-1
                     int prevSel = (sel <= 0) ? (count - 1) : sel - 1;
                     SendMessageW(hListBox, LB_SETCURSEL, prevSel, 0);
                     SendMessageW(hwnd, WM_COMMAND, ID_BTN_PLAY, 0);
